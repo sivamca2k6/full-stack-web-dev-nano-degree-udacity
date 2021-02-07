63c324d25cfbb43e8f0fe58c1f7364878acd7147
SIVAKUMAR JAGANATHAN <sivamca2k6.work@gmail.com>
2021-02-07 09:53:47 +0800

    migrations lesson completed

---
+++ lessons/module1_db_interaction/todo_app/app_migration_add_column.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template,jsonify,request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/ToDo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+migrate = Migrate(app,db)
+
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=False, default=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+#db.create_all()
+
+#print(Todo.query.first())
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  description = request.get_json()['description']
+  todo = Todo(description=description)
+  db.session.add(todo)
+  db.session.commit()
+  return jsonify({
+    'description': todo.description
+  })
+
+
+@app.route('/')
+def index():
+  return render_template('index_ajax.html', data=Todo.query.all())
