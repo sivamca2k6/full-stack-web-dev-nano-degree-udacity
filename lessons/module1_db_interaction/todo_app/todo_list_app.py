66366d0dd52cf837ebfaf27f28693193c1e235b8
SIVAKUMAR JAGANATHAN <sivamca2k6.work@gmail.com>
2021-02-11 22:41:53 +0800

    review changes for my todoapp aganist solution one

--- lessons/module1_db_interaction/todo_app/todo_list_app.py
+++ lessons/module1_db_interaction/todo_app/todo_list_app.py
@@ -1,4 +1,6 @@
+import sys
 from flask import Flask, render_template, url_for, request, redirect,jsonify
+from werkzeug.exceptions import abort
 app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, current
@@ -37,14 +39,27 @@ class ToDoList(db.Model):
 
 @app.route('/todos/create', methods=['POST']) #route method
 def create_todo():#route handler
-  description = request.get_json()['description']
-  selected_list_id = request.get_json()['selected_list_id']
-  todo = Todo(description=description,list_id = selected_list_id)
-  db.session.add(todo)
-  db.session.commit()
-  return jsonify({
-    'description': todo.description
-  })
+  error = False
+  body = {}
+  try:
+    description = request.get_json()['description']
+    selected_list_id = request.get_json()['selected_list_id']
+    todo = Todo(description=description,list_id = selected_list_id)
+    db.session.add(todo)
+    db.session.commit()
+    body['id'] = todo.id
+    body['complete'] = todo.completed
+    body['description'] = todo.description
+  except():
+      db.session.rollback()
+      error = True
+      print(sys.exc_info())
+  finally:
+      db.session.close()
+  if error:
+    abort(500)
+  else:
+    return jsonify(body)
 
 
 @app.route('/todos/<todo_id>/update_completed', methods=['POST'])
@@ -62,6 +77,7 @@ def update_completed_todo(todo_id):
 
 @app.route('/delete_todos/<todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
+  error = False
   try:
     Todo.query.filter_by(id=todo_id).delete()
     #todo = Todo.query.get(todo_id) ALTERNATE WAY
@@ -69,9 +85,15 @@ def delete_todo(todo_id):
     db.session.commit()
   except:
     db.session.rollback()
+    error = True
   finally:
     db.session.close()
-  return jsonify({ 'success': True })
+
+  if error:
+      abort(500)
+  else:
+      return jsonify({'success': True})
+  
 
 
 @app.route('/todo_list/create', methods=['POST']) #route method
@@ -89,32 +111,46 @@ def index():
   
 @app.route('/lists/<todo_list_id>') 
 def index_list(todo_list_id): 
-  #print(Todo.query.filter_by(list_id = todo_list_id).order_by('id').all())  
-  selected_list_id = todo_list_id
-  return render_template('todo_list_index.html',
-   selected_list_name= ToDoList.query.get(todo_list_id).listname,
-   selected_list_id= todo_list_id,
-   todolists= ToDoList.query.all(),
-   todos=Todo.query.filter_by(list_id = selected_list_id).order_by('id').all()
-   )
+  error = False
+  try:
+    #print(Todo.query.filter_by(list_id = todo_list_id).order_by('id').all())  
+    selected_list_id = todo_list_id
+    return render_template('todo_list_index.html',
+          selected_list_name= ToDoList.query.get(todo_list_id).listname,
+          selected_list_id= todo_list_id,
+          todolists= ToDoList.query.all(),
+          todos=Todo.query.filter_by(list_id = selected_list_id).order_by('id').all()
+          )
+  except:
+    error = True
+  if error:
+        abort(500)
 
 @app.route('/todo_list/<todolistId>/update_completed', methods=['POST'])
 def update_completed_todo_list(todolistId):
+  error = False
   try:
     completed = request.get_json()['completed']
+   
     todolist_query =  ToDoList.query.options(joinedload('todos')).get(todolistId) # get all the joined data from sql db
     for todo in todolist_query.todos:
       todo.completed = completed
+    
     db.session.commit()
   except:
     db.session.rollback()
+    error = True
   finally:   
     db.session.close()
-  return redirect(url_for('index_list',todo_list_id=todolistId)) 
-  #return jsonify({'redirect': '/lists/1'}) #for ajax response
+
+  if error:
+        abort(500)
+  else:
+        return '', 200
 
 @app.route('/delete_todo_list/<todolistId>', methods=['DELETE'])
 def delete_todo_list(todolistId):
+  error = False
   try:
     #ToDoList.query.filter_by(id=todolistId).delete()
     #todo = Todo.query.get(todo_id) ALTERNATE WAY
@@ -123,7 +159,11 @@ def delete_todo_list(todolistId):
     #print(ToDoList.query.all())
   except:
     db.session.rollback()
+    error = True
   finally:
     db.session.close()
-  return jsonify({ 'success': True }) 
+  if error:
+      abort(500)
+  else:
+      return jsonify({'success': True})
     
